8a928cfb42992a1418a5da7da41a4fbd37b154e3
Egon Ruiter <e.m.g.ruiter@students.uu.nl>
2026-09-06 21:01:58 +0200

    revert(frontend): remove usePolling to prevent high CPU freeze on dev server

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
-    watch: {
-      usePolling: true,
-    },
   },
   ssr: {
     noExternal: ["@react-keycloak/web", "keycloak-js"],
